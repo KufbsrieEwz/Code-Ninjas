12ea776b3fe5f7f104e99d3e5df65bb7ec1ef537
KufbsrieEwz <komododragon026@gmail.com>
2025-03-16 18:44:40 -0400

    Update script.js

--- Kahoot!/script.js
+++ Kahoot!/script.js
@@ -27,15 +27,15 @@ class Vector2 {
         let relativeThis = this.add(thatMin.multiply(-1))
         return (
             (
-                0 <= relativeThis.x
+                0 < relativeThis.x
                 &&
-                relativeThis.x <= thatMax.x
+                relativeThis.x < thatMax.x
             )
             &&
             (
-                0 <= relativeThis.y
+                0 < relativeThis.y
                 &&
-                relativeThis.y <= thatMax.y
+                relativeThis.y < thatMax.y
             )
         )
     }
@@ -87,8 +87,16 @@ function write(text, pos, r, g, b, a) {
 function clear() {
     c.clearRect(0, 0, window.innerWidth, window.innerHeight)
 }
-let question
-let correct
+let gameState
+/** gameState legend:
+ * 'asking': a question is being presented to the ninja
+ * 'answering': the ninja has been presented with potential answers and is picking one
+ * 'answered': the ninja has picked an answer and is waiting to see if they got it correct
+ * 'responded': the ninja is being told if they got it correct or not along with other gameData that is relevant
+ */
+// the string values can be replaced with numbers later on
+// the strings are just for readability
+let question = ''
 let answers = {
     A: 'A',
     B: 'B',
@@ -102,19 +110,53 @@ function run() {
     drawRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width, canvas.height).multiply(0.5), 95, 160, 223, 1)
     drawRect(new Vector2(0, canvas.height/2), new Vector2(canvas.width, canvas.height).multiply(0.5), 245, 195, 68, 1)
     drawRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width, canvas.height).multiply(0.5), 124, 188, 78, 1)
-    if (mouse.inBoundsRect(Vector2.zero, Vector2.zero.add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+    switch (gameState) {
+        case 'asking':
+            // draw the question prompt
+            break
+        case 'answering':
+            // draw a header of the question
+            discolour(mouse)
+            break
+        case 'answered':
+            // draw a waiting screen so they don't get bored
+            break
+        case 'responded':
+            // draw important information such as if they got it correct and how many points they got
+            break
+    }
+}
+function click(pos) {
+    if (pos.inBoundsRect(Vector2.zero, Vector2.zero.add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+        // A
+        console.log('A')
+    }
+    if (pos.inBoundsRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width/2, 0).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+        // B
+        console.log('B')
+    }
+    if (pos.inBoundsRect(new Vector2(0, canvas.height/2), new Vector2(0, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+        // C
+        console.log('C')
+    }
+    if (pos.inBoundsRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width/2, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+        // D
+        console.log('D')
+    }
+}
+function discolour(pos) {
+    if (pos.inBoundsRect(Vector2.zero, Vector2.zero.add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
         drawRect(Vector2.zero, new Vector2(canvas.width, canvas.height).multiply(0.5), 0, 0, 0, 0.2)
     }
-    if (mouse.inBoundsRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width/2, 0).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+    if (pos.inBoundsRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width/2, 0).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
         drawRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width, canvas.height).multiply(0.5), 0, 0, 0, 0.2)
     }
-    if (mouse.inBoundsRect(new Vector2(0, canvas.height/2), new Vector2(0, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+    if (pos.inBoundsRect(new Vector2(0, canvas.height/2), new Vector2(0, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
         drawRect(new Vector2(0, canvas.height/2), new Vector2(canvas.width, canvas.height).multiply(0.5), 0, 0, 0, 0.2)
     }
-    if (mouse.inBoundsRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width/2, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
+    if (pos.inBoundsRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width/2, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
         drawRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width, canvas.height).multiply(0.5), 0, 0, 0, 0.2)
     }
-    
 }
 setInterval(run, 1)
 document.addEventListener("mousemove", function() {
@@ -122,16 +164,5 @@ document.addEventListener("mousemove", function() {
 })
 document.addEventListener("click", function (event) {
     mouse = new Vector2(event.x, event.y)
-    if (mouse.inBoundsRect(Vector2.zero, Vector2.zero.add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
-        // A
-    }
-    if (mouse.inBoundsRect(new Vector2(canvas.width/2, 0), new Vector2(canvas.width/2, 0).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
-        // B
-    }
-    if (mouse.inBoundsRect(new Vector2(0, canvas.height/2), new Vector2(0, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
-        // C
-    }
-    if (mouse.inBoundsRect(new Vector2(canvas.width/2, canvas.height/2), new Vector2(canvas.width/2, canvas.height/2).add(new Vector2(canvas.width, canvas.height).multiply(0.5)))) {
-        // D
-    }
+    click(mouse)
 })
