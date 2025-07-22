53247d582685ba84a42bedf1105911146c9bb96b
KufbsrieEwz <komododragon026@gmail.com>
2025-07-22 18:43:09 -0400

    Update start.bat

--- Archaea/start.bat
+++ Archaea/start.bat
@@ -2,9 +2,9 @@
 title Start.bat - System Reset & Setup
 color 0E
 
-echo ================================================
-echo  A R C H A E A   -   S Y S T E M   I N I T
-echo ================================================
+echo =====================================================
+echo      A R C H A E A   -   S Y S T E M   I N I T
+echo =====================================================
 echo.
 
 :: Step 1 - Run delete.bat
@@ -15,12 +15,21 @@ call "%~dp0delete.bat"
 echo Running setup.bat...
 call "%~dp0setup.bat"
 
-:: Step 3 - Clean up the Archaea folder
-echo Cleaning up Archaea folder...
-:: Delete all files
-del /f /q "%~dp0*.*"
-:: Delete all subfolders
-for /d %%D in ("%~dp0*") do rd /s /q "%%D"
+:: Step 3 - Prepare to clean up the Archaea folder
+echo Preparing Archaea folder cleanup...
+
+:: Change directory to avoid being inside Archaea
+cd /d "%USERPROFILE%\Downloads"
+
+:: Create temporary cleanup script
+set CLEANUP_SCRIPT=%TEMP%\cleanup.bat
+echo @echo off > "%CLEANUP_SCRIPT%"
+echo timeout /t 3 /nobreak > "%CLEANUP_SCRIPT%"
+echo rd /s /q "%~dp0" >> "%CLEANUP_SCRIPT%"
+echo del "%%~f0" >> "%CLEANUP_SCRIPT%"
+
+:: Run the cleanup script
+start "" "%CLEANUP_SCRIPT%"
 
 echo.
 echo Archaea setup complete. System is ready.
